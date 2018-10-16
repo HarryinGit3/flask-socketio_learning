3ecccf722b2be36d5a66027ec1754d5d2fa4ab0a
Harryin <harryin123@qq.com>
2018-10-16 22:19:45 +0800

    项目初始化

---
+++ s.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import random
+async_mode = None
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# 使用json数据
+@socketio.on('json')
+def handle_json(json):
+    print('received json:' + str(json))
+
+
+@socketio.on('my_event')
+def get_message(message):
+    print(message['data'])
+    while True:
+        socketio.sleep(5)
+        t = random_int_list(1, 100, 10)
+        socketio.emit('server_response', t)
+
+def random_int_list(start, stop, length):
+    start, stop = (int(start), int(stop)) if start <= stop else (int(stop),
+                                                                 int(start))
+    length = int(abs(length)) if length else 0
+    random_list = []
+    for i in range(length):
+        random_list.append(random.randint(start, stop))
+    return random_list
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=3300)
