a2843be9e5df6eda60c0963e27ff6b46f563d092
Harryin <harryin123@qq.com>
2018-10-22 14:28:03 +0800

    前后端连接成功

--- s.py
+++ s.py
@@ -6,19 +6,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
 # 使用json数据
 @socketio.on('json')
 def handle_json(json):
     print('received json:' + str(json))
 
 
+#handle the default namespace
+
+
 @socketio.on('my_event')
 def get_message(message):
     print(message['data'])
     while True:
-        socketio.sleep(5)
+        socketio.sleep(15)
         t = random_int_list(1, 100, 10)
         socketio.emit('server_response', t)
+
 
 def random_int_list(start, stop, length):
     start, stop = (int(start), int(stop)) if start <= stop else (int(stop),
